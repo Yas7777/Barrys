b58c188ed2497296e428e01127e4337d8ddefaa1
Yas7777 <yasmeen.azeem@hotmail.com>
2023-06-25 18:16:55 -0400

    Added error checks for dates

--- barrys.cpp
+++ barrys.cpp
@@ -1,17 +1,20 @@
 #include "barrys.h"
 #include <iostream>
-#include <fstream>
 #include <algorithm>
-#include <vector>
 #include <map>
+#include <numeric>
+#include <chrono>
+#include <iostream>
 
 using namespace std;
 
-// Prompting User to add a day
-pair<string, string> day() {
 
+// Prompting User to add a day and its associated focus
+pair<string, string> day() {
     string input;
-    cout << " Please enter the day";
+    cout << "Please enter the day: ";
+    getline(cin, input);
+
     map<string, string> workoutDays {
             {"Monday", "Arms & Abs"},
             {"Tuesday", "Full Body-Lower Focus "},
@@ -22,72 +25,104 @@ pair<string, string> day() {
             {"Sunday", "Total Body"}
     };
 
-    if (input == "Monday" || "Tuesday" || "Wednesday" || "Thursday" || "Friday" || "Saturday" || "Sunday") {
-        return make_pair(input, workoutDays[input]);
-    } else {
+    // Error Check else return the pair
+    if (workoutDays.count(input) == 0) {
         cout << "Invalid input. Please enter a valid day of the week. \n";
-        return make_pair("", "");
+        exit(1);
+    } else {
+        // Key exists in the map
+        return make_pair(input, workoutDays[input]);
     }
 }
 
 // Prompting User to enter the date
-    string date() {
-        string inputDate;
-        cout << "Please enter the date of your workout: ";
-        getline(cin, inputDate);
-        return inputDate;
+string date() {
+    string inputDate;
+    cout << "Please enter the date of your workout: ";
+    getline(cin, inputDate);
+
+// Check if the input date has the correct format
+// The size of 10 ==  format "DD/MM/YYYY"
+
+    if (inputDate.size() != 10 || inputDate[3] != '/' || inputDate[6] != '/') {
+        cout << "Invalid date. Please enter a date in the format YYYY/MM/DD. \n";
+        exit(1);
     }
+    // Basic Check for valid dates
+    string validDay = inputDate.substr(0,2);
+    string validMonth = inputDate.substr(3,2);
+    string validYear = inputDate.substr(6,4);
+    int day = stoi(validDay);
+    int month = stoi (validMonth);
+    int year = stoi(validYear);
+
+    if (year < 1900 || year > 2100 || month < 1 || month > 12 || day < 1 || day > 31) {
+        cout << "Invalid date. Please enter a valid date. \n";
+        exit(1);
+    }
+    return inputDate;
+}
 
 // Prompting User to add the instructors name
-    string workoutInstructor() {
-        string inputWorkoutInstructor;
-        cout << "Please enter the name of your instructor";
-        getline(cin, inputWorkoutInstructor);
-        return inputWorkoutInstructor;
-    }
+string workoutInstructor() {
+    string inputWorkoutInstructor;
+    cout << "Please enter the name of your instructor: ";
+    getline(cin, inputWorkoutInstructor);
+    return inputWorkoutInstructor;
+}
 
 // Prompting user to add Treadmill data
-    string data() {
-        string inputIncline;
-        cout << "Was there an incline? (y/n only): ";
-        cin >> inputIncline;
-        if (inputIncline == "Y" || inputIncline == "y" || inputIncline == "n" || inputIncline == "N") {
-        } else {
-            cout << "Invalid input ";
-        }
-        return inputIncline;
+string data() {
+    string inputIncline;
+    cout << "Was there an incline? (y/n only): ";
+    cin >> inputIncline;
+    if (inputIncline == "Y" || inputIncline == "y" || inputIncline == "n" || inputIncline == "N") {
+    } else {
+        cout << "Invalid input ";
     }
+    cin.ignore();  // Ignore the newline character left by cin
+    return inputIncline;
+}
 
 // Prompting user to add Treadmill data
 int treadmillSprint() {
-    int inputSprint;
-    cout << "What was your fastest spring level at?";
-    cin >> inputSprint;
-    return inputSprint;
+    string input;
+    cout << "What was your fastest spring level at? ";
+    getline(cin, input);
+    return stoi(input);
 }
-// Delete workout
+
 void deleteWorkout(const string& targetDate) {
     ifstream file("WorkoutData.txt");
     ofstream tempFile("temp.txt");
     string line;
-    bool deleteLines = false;
+    vector<string> workout;
 
     while (getline(file, line)) {
-        // Detect start of a workout
-        if (line.substr(0, 15) == "Day of the Week") {
-            if (line.find(targetDate) != string::npos) {
-                deleteLines = true;
+        if (line.find("Day of the Week: ") == 0) {  // Start of a new workout
+            // If the last workout doesn't contain the target date, write it to the temp file
+            if (!workout.empty()) {
+                string workoutStr = accumulate(workout.begin(), workout.end(), string(""));
+                if (workoutStr.find(targetDate) == string::npos) {
+                    for (const auto& l : workout) {
+                        tempFile << l << "\n";
+                    }
+                }
+                workout.clear();
             }
         }
-        // Detect end of a workout
-        if (line.substr(0, 10) == "==========") {
-            deleteLines = false;
-        }
-        // Write lines not marked for deletion to the temp file
-        if (!deleteLines) {
-            tempFile << line << "\n";
+        workout.push_back(line);
+    }
+    // Don't forget to handle the last workout
+    if (!workout.empty()) {
+        string workoutStr = accumulate(workout.begin(), workout.end(), string(""));
+        if (workoutStr.find(targetDate) == string::npos) {
+            for (const auto& l : workout) {
+                tempFile << l << "\n";
+            }
         }
     }
+
     file.close();
     tempFile.close();
 
@@ -97,39 +132,42 @@ void deleteWorkout(const string& targetDate) {
 }
 
 
+
 // Main function
 int main() {
-    string dayOfTheWeek = day();
-    string workoutDate = date();
-    string instructorName = workoutInstructor();
-    string inclineData = data();
-    int sprintData = treadmillSprint();
-
-    // Open a file in write mode.
-    ofstream outfile;
-    outfile.open("WorkoutData.txt");
-
-    // Write the data to the file.
-    outfile << "Day of the Week: " << dayOfTheWeek << "\n";
-    outfile << "Workout Date: " << workoutDate << "\n";
-    outfile << "Instructor Name: " << instructorName << "\n";
-    outfile << "Incline Data: " << inclineData << "\n";
-    outfile << "Sprint Data: " << sprintData << "\n";
+    string input;
+    int choice;
+    cout << "Enter your choice (1 for adding a workout, 2 for deleting a workout, 3 for exit): ";
+    getline(cin, input);
+    choice = stoi(input);
 
     if(choice == 1) {
-        // Your existing code to add a workout
+        pair<string, string> dayOfWorkout = day();
+        string workoutDate = date();
+        string instructorName = workoutInstructor();
+        string inclineData = data();
+        int sprintData = treadmillSprint();
+
+        ofstream outfile;
+        outfile.open("WorkoutData.txt", ios::app); // Append to the existing file
+
+        outfile << "Day of the Week: " << dayOfWorkout.first << "\n";
+        outfile << "Workout Focus: " << dayOfWorkout.second << "\n";
+        outfile << "Workout Date: " << workoutDate << "\n";
+        outfile << "Instructor Name: " << instructorName << "\n";
+        outfile << "Incline Data: " << inclineData << "\n";
+        outfile << "Sprint Data: " << sprintData << "\n";
+        outfile << "==========\n";
+
+        outfile.close();
     } else if (choice == 2) {
         string targetDate;
         cout << "Enter the date of the workout you want to delete: ";
-        cin >> targetDate;
+        getline(cin, targetDate);
         deleteWorkout(targetDate);
     } else if (choice == 3) {
         return 0;
     }
 
-    // Close the file
-    outfile.close();
     return 0;
-
 }
-
